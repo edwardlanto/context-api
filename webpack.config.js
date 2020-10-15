04076cbca0dff7f681ccec90afc2de45d2e1ac57
Edward Lanto <elanto@hotmail.com>
2020-10-14 21:30:52 -0700

    add public url in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     port: 9000,
     historyApiFallback: true,
-    contentBase: '/',
+    contentBase: process.env.PUBLIC_URL,
     hot: true
   },
   devtool: false,
